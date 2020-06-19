d484305f26a65ee8eb8abfb9dee7c575ba7f343e
multicus <zimmerjan@outlook.com>
2020-06-19 14:52:37 +0200

    doc: add comment

--- src/main/java/com/multicus/steamApp/marketapi/WrongQueryException.java
+++ src/main/java/com/multicus/steamApp/marketapi/WrongQueryException.java
@@ -8,6 +8,12 @@ import java.util.Arrays;
  * When the parameters in a query are invalid
  */
 public class WrongQueryException extends SteamException {
+    /**
+     * Constructor that takes the apiname that created the exception
+     * and the query parameters
+     * @param apiName
+     * @param query
+     */
     public WrongQueryException(String apiName, String... query) {
         super("Could not execute api:\t" + apiName + "\twith query:\t" + Arrays.toString(query));
     }
